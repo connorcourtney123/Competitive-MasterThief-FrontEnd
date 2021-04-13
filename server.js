c199b37a2c50e8ec7a00fa79aca9a07afbe4b034
ConnorCourtney <connorcourtney123@gmail.com>
2021-04-13 11:54:37 -0400

    tuesday morning commit

--- server.js
+++ server.js
@@ -3,12 +3,18 @@ const app = express()
 const rowdy = require('rowdy-logger') //for rowdy-logger
 const routesReport = rowdy.begin(app)//for rowdy-logger
 
+const PORT = process.env.PORT || 3001
 
 app.use(express.json())//allow for use of req.body
 
 
 //listen on port 3001
-app.listen(process.env.PORT || 3001, () => {
+app.listen(PORT, () => {
   console.log('the server is listening!')
 	routesReport.print()
 })
+// let port = process.env.PORT;
+// if (port == null || port == "") {
+//   port = 3001;
+// }
+// app.listen(port);
